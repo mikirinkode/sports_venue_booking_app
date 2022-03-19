3a2062d263074cf3645e2c5d4da2026ea21d4461
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-03-19 23:49:42 +0700

    adding flutter launcher icon dependency

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import '../theme.dart';
 import '../widget/facility_widget.dart';
 
