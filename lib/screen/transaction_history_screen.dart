77b020818f0bf4d333350bc02b947e11232250f6
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-03-20 10:55:41 +0700

    update transaction history screen

---
+++ lib/screen/transaction_history_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../theme.dart';
+
+class HistoryScreen extends StatelessWidget {
+  const HistoryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: lightBlue100,
+        appBar: AppBar(title: Text("Transactions"), elevation: 0.0,),
+        body: noTransaction(context));
+  }
+
+  Widget noTransaction(context) {
+    return Center(
+      child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+          Image.asset("assets/images/no_transaction_illustration.png", width: 150,),
+          Text(
+            "No Transactions, yet.",
+            style: titleTextStyle.copyWith(color: darkBlue300),
+          ),
+          const SizedBox(height: 8.0,),
+          Text(
+            "You have never placed an order. Let's explore the field near you.",
+            style: descTextStyle,
+              textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 32.0,),
+          TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+              label: Text("Search a Field", style: buttonTextStyle.copyWith(color: primaryColor),))
+    ],
+    ),
+        ),
+    );
+  }
+}
