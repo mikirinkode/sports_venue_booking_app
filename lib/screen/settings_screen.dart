b400607fc9ca9400de79868e483b9ae20ecf6fd5
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-03-25 13:58:12 +0700

    update settings screen: touchable

--- lib/screen/settings_screen.dart
+++ lib/screen/settings_screen.dart
@@ -20,8 +20,7 @@ class SettingsScreen extends StatelessWidget {
         ),
         body: SingleChildScrollView(
           child: Padding(
-            padding:
-                const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -56,17 +55,20 @@ class SettingsScreen extends StatelessWidget {
                           "Muhammad Wafa",
                           style: subTitleTextStyle,
                         ),
-                        SizedBox(height: 8,),
+                        SizedBox(
+                          height: 8,
+                        ),
                         Container(
-                          padding: EdgeInsets.all(4),
-                          decoration: BoxDecoration(
-                            color: primaryColor100.withOpacity(0.5),
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(
-                              color: primaryColor500
-                            )
-                          ),
-                            child: Text("Premium", style: descTextStyle.copyWith(color: primaryColor500),))
+                            padding: EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                                color: primaryColor100.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(color: primaryColor500)),
+                            child: Text(
+                              "Premium",
+                              style: descTextStyle.copyWith(
+                                  color: primaryColor500),
+                            ))
                       ],
                     ),
                   ],
@@ -74,200 +76,200 @@ class SettingsScreen extends StatelessWidget {
                 SizedBox(
                   height: 32,
                 ),
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Payment",
-                        style: subTitleTextStyle.copyWith(color: primaryColor500),
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle, color: colorWhite),
-                            child: Icon(
-                              CupertinoIcons.creditcard_fill,
-                              size: 24,
-                              color: darkBlue300,
+                Text(
+                  "Payment",
+                  style: subTitleTextStyle.copyWith(color: primaryColor500),
+                ),
+                InkWell(
+                  onTap: () {},
+                  splashColor: primaryColor100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: colorWhite),
+                          child: Icon(
+                            CupertinoIcons.creditcard_fill,
+                            size: 24,
+                            color: darkBlue300,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Current Payment method",
+                              style: normalTextStyle,
                             ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              "Not Set",
+                              style: descTextStyle,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Other",
+                  style: subTitleTextStyle.copyWith(color: primaryColor500),
+                ),
+                InkWell(
+                  onTap: () {},
+                  splashColor: primaryColor100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: colorWhite),
+                          child: Icon(
+                            CupertinoIcons.moon_circle,
+                            size: 24,
+                            color: darkBlue300,
                           ),
-                          SizedBox(
-                            width: 16,
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Theme",
+                              style: normalTextStyle,
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              "Light",
+                              style: descTextStyle,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  splashColor: primaryColor100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: colorWhite),
+                          child: Icon(
+                            Icons.language_rounded,
+                            size: 24,
+                            color: darkBlue300,
                           ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Current Payment method",
-                                style: normalTextStyle,
-                              ),
-                              SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                "Not Set",
-                                style: descTextStyle,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Language",
+                              style: normalTextStyle,
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              "English",
+                              style: descTextStyle,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 SizedBox(
                   height: 32,
                 ),
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Other",
-                        style: subTitleTextStyle.copyWith(color: primaryColor500),
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle, color: colorWhite),
-                            child: Icon(
-                              CupertinoIcons.moon_circle,
-                              size: 24,
-                              color: darkBlue300,
+                Text(
+                  "App Version",
+                  style: subTitleTextStyle.copyWith(color: primaryColor500),
+                ),
+                InkWell(
+                  onTap: () {
+                    _showSnackBar(context, "Newest Version");
+                  },
+                  splashColor: primaryColor100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: colorWhite),
+                          child: Icon(
+                            CupertinoIcons.info_circle_fill,
+                            size: 24,
+                            color: darkBlue300,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Spod - Sports Field Booking App",
+                              style: normalTextStyle,
                             ),
-                          ),
-                          SizedBox(
-                            width: 16,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Theme",
-                                style: normalTextStyle,
-                              ),
-                              SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                "Light",
-                                style: descTextStyle,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle, color: colorWhite),
-                            child: Icon(
-                              Icons.language_rounded,
-                              size: 24,
-                              color: darkBlue300,
+                            SizedBox(
+                              height: 8,
                             ),
-                          ),
-                          SizedBox(
-                            width: 16,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Current Payment method",
-                                style: normalTextStyle,
-                              ),
-                              SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                "Not Set",
-                                style: descTextStyle,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
+                            Text(
+                              "Alpha Version",
+                              style: descTextStyle,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-
                 SizedBox(
-                  height: 32,
+                  height: 16,
                 ),
-
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "App Version",
-                        style: subTitleTextStyle.copyWith(color: primaryColor500),
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle, color: colorWhite),
-                            child: Icon(
-                              CupertinoIcons.info_circle_fill,
-                              size: 24,
-                              color: darkBlue300,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 16,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Spod - Sports Field Booking App",
-                                style: normalTextStyle,
-                              ),
-                              SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                "Alpha Version",
-                                style: descTextStyle,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 32,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Created with ", style: normalTextStyle,),
+                    Text(
+                      "Created with ",
+                      style: normalTextStyle,
+                    ),
                     SizedBox(
                       width: 4,
                     ),
@@ -278,7 +280,10 @@ class SettingsScreen extends StatelessWidget {
                     SizedBox(
                       width: 4,
                     ),
-                    Text("and", style: normalTextStyle,),
+                    Text(
+                      "and",
+                      style: normalTextStyle,
+                    ),
                     SizedBox(
                       width: 4,
                     ),
@@ -292,5 +297,16 @@ class SettingsScreen extends StatelessWidget {
             ),
           ),
         ));
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(SnackBar(
+      content: Text(message),
+      margin: EdgeInsets.all(16),
+      behavior: SnackBarBehavior.floating,
+      duration: Duration(seconds: 2),
+      // margin: EdgeInsets.all(16),
+    ));
   }
 }
