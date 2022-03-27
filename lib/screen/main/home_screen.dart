6f500d0b80a7b1cd71752f30b83746cf1eee6957
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-03-27 10:46:28 +0700

    clean up code

--- lib/screen/main/home_screen.dart
+++ lib/screen/main/home_screen.dart
@@ -1,24 +1,19 @@
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:spod_app/model/sport_field.dart';
 import 'package:spod_app/screen/search_screen.dart';
 import 'package:spod_app/theme.dart';
 import 'package:spod_app/utils/dummy_data.dart';
-
-import '../model/sport_field.dart';
-import '../widget/category_card.dart';
-import '../widget/sport_field_card.dart';
+import 'package:spod_app/widget/category_card.dart';
+import 'package:spod_app/widget/sport_field_card.dart';
 
 class HomeScreen extends StatelessWidget {
-  List<SportField> fieldList = sportFieldList;
-  final List<int> numberList = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<SportField> fieldList = recommendedSportField;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: lightBlue100,
+      backgroundColor: backgroundColor,
       body: Column(
         children: [
           header(context),
@@ -27,12 +22,17 @@ class HomeScreen extends StatelessWidget {
               padding: EdgeInsets.zero,
               children: [
                 Container(
-                  padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
-                  child: Text("Let's Have Fun and \nBe Healty!", style: greetingTextStyle,),
+                  padding:
+                      const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
+                  child: Text(
+                    "Let's Have Fun and \nBe Healty!",
+                    style: greetingTextStyle,
+                  ),
                 ),
                 CategoryListView(),
                 Padding(
-                  padding: const EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0),
+                  padding:
+                      const EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -40,11 +40,16 @@ class HomeScreen extends StatelessWidget {
                         "Recommended Fields",
                         style: subTitleTextStyle,
                       ),
-                      TextButton(onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context) {
-                          return SearchScreen();
-                        }));
-                      }, child: Text("Show All"))
+                      TextButton(
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return SearchScreen(
+                                selectedDropdownItem: "All",
+                              );
+                            }));
+                          },
+                          child: const Text("Show All"))
                     ],
                   ),
                 ),
@@ -52,8 +57,8 @@ class HomeScreen extends StatelessWidget {
                 Column(
                     children: fieldList
                         .map((fieldEntity) => SportFieldCard(
-                      field: fieldEntity,
-                    ))
+                              field: fieldEntity,
+                            ))
                         .toList()),
               ],
             ),
@@ -65,7 +70,7 @@ class HomeScreen extends StatelessWidget {
 
   Widget header(context) {
     return Container(
-      padding: EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(16.0),
       child: SafeArea(
         // SEARCH Icon
         child: Row(
@@ -76,8 +81,8 @@ class HomeScreen extends StatelessWidget {
               children: [
                 Container(
                   width: 55,
-                  height:55,
-                  decoration: BoxDecoration(
+                  height: 55,
+                  decoration: const BoxDecoration(
                     shape: BoxShape.circle,
                     color: Colors.white,
                     image: DecorationImage(
@@ -86,7 +91,9 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(width: 16,),
+                const SizedBox(
+                  width: 16,
+                ),
                 Column(
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -94,6 +101,9 @@ class HomeScreen extends StatelessWidget {
                     Text(
                       "Welcome back,",
                       style: descTextStyle,
+                    ),
+                    const SizedBox(
+                      height: 4,
                     ),
                     Text(
                       "Muhammad Wafa",
@@ -106,15 +116,16 @@ class HomeScreen extends StatelessWidget {
             Container(
               decoration: BoxDecoration(
                   color: primaryColor500,
-                  borderRadius:
-                  BorderRadius.circular(borderRadiusSize)),
+                  borderRadius: BorderRadius.circular(borderRadiusSize)),
               child: IconButton(
                 onPressed: () {
                   Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return SearchScreen();
+                    return SearchScreen(
+                      selectedDropdownItem: "",
+                    );
                   }));
                 },
-                icon: Icon(Icons.search, color: colorWhite),
+                icon: const Icon(Icons.search, color: colorWhite),
               ),
             )
           ],
