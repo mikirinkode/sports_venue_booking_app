8ce7a9b98978dffba441361c3f27205063665121
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-04-05 14:57:58 +0700

    move checkout btn to bottom nav

--- lib/screen/detail/checkout_screen.dart
+++ lib/screen/detail/checkout_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
-import 'package:spod_app/main.dart';
 import 'package:spod_app/model/checkbox_state.dart';
 import 'package:spod_app/model/field_order.dart';
 import 'package:spod_app/model/sport_field.dart';
+import 'package:spod_app/screen/main/main_screen.dart';
 import 'package:spod_app/theme.dart';
 import 'package:spod_app/utils/dummy_data.dart';
-
 
 class CheckoutScreen extends StatefulWidget {
   SportField field;
@@ -20,7 +20,7 @@ class CheckoutScreen extends StatefulWidget {
 class _CheckoutScreenState extends State<CheckoutScreen> {
   final TextEditingController _controller = TextEditingController();
   DateTime _dateTime = DateTime.now();
-  final dateFormat = DateFormat("E, dd MM yyyy");
+  final dateFormat = DateFormat("EEEE, dd MMM yyyy");
   final availableBookTime = [
     CheckBoxState(title: "07.00 - 08.00"),
     CheckBoxState(title: "09.00 - 10.00"),
@@ -40,126 +40,157 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Field Name",
-              style: subTitleTextStyle,
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: colorWhite,
+              statusBarIconBrightness: Brightness.dark,
             ),
-            const SizedBox(
-              height: 8,
-            ),
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                  border: Border.all(color: primaryColor100, width: 2),
-                  color: lightBlue100,
-                  borderRadius: BorderRadius.circular(borderRadiusSize)),
-              child: Text(widget.field.name,
-                  style: normalTextStyle.copyWith(fontWeight: FontWeight.w600)),
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Text(
-              "Pick a date",
-              style: subTitleTextStyle,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            InkWell(
-              onTap: () {
-                _selectDate();
-              },
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                    border: Border.all(color: primaryColor100, width: 2),
-                    color: lightBlue100,
-                    borderRadius: BorderRadius.circular(borderRadiusSize)),
-                child: Text(
-                  _dateTime == null
-                      ? "date not selected.."
-                      : dateFormat.format(_dateTime).toString(),
-                  style: normalTextStyle,
-                ),
+            title: Text("Checkout"),
+            backgroundColor: colorWhite,
+            centerTitle: true,
+            foregroundColor: primaryColor500,
+          ),
+          SliverPadding(
+            padding:
+                const EdgeInsets.only(right: 24, left: 24, bottom: 24, top: 8),
+            sliver: SliverList(
+                delegate: SliverChildListDelegate([
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Venue Name",
+                    style: subTitleTextStyle,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: primaryColor100, width: 2),
+                        color: lightBlue100,
+                        borderRadius: BorderRadius.circular(borderRadiusSize)),
+                    child: Text(widget.field.name,
+                        style: normalTextStyle.copyWith(
+                            fontWeight: FontWeight.w600)),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Text(
+                    "Pick a date",
+                    style: subTitleTextStyle,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      _selectDate();
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: primaryColor100, width: 2),
+                          color: lightBlue100,
+                          borderRadius:
+                              BorderRadius.circular(borderRadiusSize)),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            _dateTime == null
+                                ? "date not selected.."
+                                : dateFormat.format(_dateTime).toString(),
+                            style: normalTextStyle,
+                          ),
+                          const Icon(
+                            Icons.album_outlined,
+                            color: primaryColor500,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Text(
+                    "Pick a Time",
+                    style: subTitleTextStyle,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  ...availableBookTime.map(buildSingleCheckBox).toList(),
+                ],
               ),
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Text(
-              "Pick a Time",
-              style: subTitleTextStyle,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            ...availableBookTime.map(buildSingleCheckBox).toList(),
-            const Spacer(),
-            Row(
+            ])),
+          )
+        ],
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: const BoxDecoration(color: Colors.white),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Total:",
-                      style: descTextStyle,
-                    ),
-                    Text(
-                      "Rp $_totalBill",
-                      style: priceTextStyle,
-                    ),
-                  ],
+                Text(
+                  "Total:",
+                  style: descTextStyle,
                 ),
-                const SizedBox(
-                  width: 16,
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(100, 45),
-                          shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.circular(borderRadiusSize))),
-                      onPressed: !_enableCreateOrderBtn
-                          ? null
-                          : () {
-                              List<String> selectedTime = [];
-                              for (int i = 0;
-                                  i < availableBookTime.length;
-                                  i++) {
-                                if (availableBookTime[i].value) {
-                                  selectedTime.add(availableBookTime[i].title);
-                                }
-                              }
-                              dummyUserOrderList.add(FieldOrder(
-                                  field: widget.field,
-                                  user: "Muhammad Wafa",
-                                  selectedDate:
-                                      dateFormat.format(_dateTime).toString(),
-                                  selectedTime: selectedTime));
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return MainScreen(currentScreen: 1);
-                              }));
-                              _showSnackBar(
-                                  context, "Successfully create an order");
-                            },
-                      child: Text(
-                        "Create Order",
-                        style: buttonTextStyle,
-                      )),
+                Text(
+                  "Rp $_totalBill",
+                  style: priceTextStyle,
                 ),
               ],
-            )
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size(100, 45),
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(borderRadiusSize))),
+                  onPressed: !_enableCreateOrderBtn
+                      ? null
+                      : () {
+                          List<String> selectedTime = [];
+                          for (int i = 0; i < availableBookTime.length; i++) {
+                            if (availableBookTime[i].value) {
+                              selectedTime.add(availableBookTime[i].title);
+                            }
+                          }
+                          dummyUserOrderList.add(FieldOrder(
+                              field: widget.field,
+                              user: sampleUser,
+                              selectedDate:
+                                  dateFormat.format(_dateTime).toString(),
+                              selectedTime: selectedTime));
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      MainScreen(currentScreen: 1)),
+                              (route) => false);
+                          _showSnackBar(
+                              context, "Successfully create an order");
+                        },
+                  child: Text(
+                    "Create Order",
+                    style: buttonTextStyle,
+                  )),
+            ),
           ],
         ),
       ),
@@ -173,7 +204,6 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
       margin: const EdgeInsets.all(16),
       behavior: SnackBarBehavior.floating,
       duration: const Duration(seconds: 2),
-      // margin: EdgeInsets.all(16),
     ));
   }
 
@@ -187,8 +217,8 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
     await showDatePicker(
             context: context,
             initialDate: DateTime.now(),
-            firstDate: DateTime(2022, 3),
-            lastDate: DateTime(2022, 4))
+            firstDate: DateTime.now(),
+            lastDate: DateTime(DateTime.now().year, DateTime.now().month + 1))
         .then((value) {
       setState(() {
         _dateTime = value!;
